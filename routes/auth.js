26e2cb7d8f87b91937ec586a0a3508f134fa3b7b
madhur <mad@gmail.com>
2023-04-21 19:23:02 +0530

    Updated auth.js

--- routes/auth.js
+++ routes/auth.js
@@ -5,6 +5,8 @@ import bcrypt from "bcryptjs";
 import jwt from "jsonwebtoken";
 const router = express.Router();
 const JWT_SECRET = 'Madhurisagoodb$oy';
+
+
 router.post('/createuser',[
   body('name', 'Enter a valid name with more than 3 characters').isLength({ min:3 }),
   body('email').isEmail(),
@@ -46,6 +48,42 @@ res.json({authtoken});
 }
 });
 
+// now we authenticate the user using POST "/api/auth/login" no login required.
+router.post('/login',[
+  body('email').isEmail(),
+  body('password', 'Password cannot be blank').exists(),
+], async (req, res) => {
+  // if there are errors return bad request and the errors 
+  const errors = validationResult(req);
+  if (!errors.isEmpty()){
+    return res.status(400).json({ errors:errors.array() });
+  }
+  const {email, password} = req.body;
+  try {
+    let user = await User.findOne({email});
+    if (!user){
+      return res.status(400).json({error: "Please try to login using correct credentials"});
+    }
+
+    const passwordCompare =await bcrypt.compare(password, user.password);
+    if (!passwordCompare){
+      return res.status(400).json({error: "Please try to login using correct credentials"});
+    }
+
+    const data = {
+      user:{
+        id: user.id
+      }
+    }
+    
+    const authtoken = jwt.sign(data,JWT_SECRET);
+    res.send({authtoken});
+
+  } catch (error) {
+    console.error(error.message);
+    res.status(500).send("Internal server error"); 
+  }
+});
 export default router;
 
 
