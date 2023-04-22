9a2dc46221f852ed2012968df592a4838d8d7f24
madhur <mad@gmail.com>
2023-04-22 14:19:53 +0530

    Added the getuser data route .

--- routes/auth.js
+++ routes/auth.js
@@ -3,9 +3,15 @@ import User from "../models/User.js";
 import { body, validationResult } from 'express-validator';
 import bcrypt from "bcryptjs";
 import jwt from "jsonwebtoken";
-const router = express.Router();
-const JWT_SECRET = 'Madhurisagoodb$oy';
+import fetchuser from "../middleware/fetchuser.js";
+import dotenv from 'dotenv';
+dotenv.config();
 
+
+const router = express.Router();
+const JWT_SECRET =  process.env.JWT_SECRET;
+
+// route 1 for the /api/auth/createuser
 
 router.post('/createuser',[
   body('name', 'Enter a valid name with more than 3 characters').isLength({ min:3 }),
@@ -48,7 +54,7 @@ res.json({authtoken});
 }
 });
 
-// now we authenticate the user using POST "/api/auth/login" no login required.
+// Route 2 now we authenticate the user using POST "/api/auth/login" no login required.
 router.post('/login',[
   body('email').isEmail(),
   body('password', 'Password cannot be blank').exists(),
@@ -84,6 +90,21 @@ router.post('/login',[
     res.status(500).send("Internal server error"); 
   }
 });
+
+// Route 3 now we get the user details using POST "/api/auth/getuser" Login required.
+router.post('/getuser',fetchuser, async (req, res) => {
+
+  try {
+    const userId = req.user.id;
+    const user = await User.findById(userId).select("-password");
+    res.send(user);
+  } catch (error) {
+    console.error(error.message);
+    res.status(500).send("Internal server error"); 
+  }
+});
+
+
 export default router;
 
 
