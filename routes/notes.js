cf1edb0e1b0731e2801dc5d96f7223060af6bd35
madhur <mad@gmail.com>
2023-04-23 16:40:17 +0530

    Users can now update their notes

--- routes/notes.js
+++ routes/notes.js
@@ -5,7 +5,7 @@ import { body, validationResult } from 'express-validator';
 const router = express.Router();
 
 
-// Route 1  To fetch all the notes that our user has created with /api/auth/fetchallnotes login required
+// Route 1  To fetch all the notes that our user has created with /api/notes/fetchallnotes login required
 router.get('/fetchnotes',fetchuser, async (req, res) => {
 try {
     const notes = await Notes.find({user: req.user.id})
@@ -18,7 +18,7 @@ try {
 
 
 
-// Route 2  To add the notes that our user has created with /api/auth/addnote login required
+// Route 2  To add the notes that our user has created with /api/notes/addnote login required
 
 router.post('/addnote',fetchuser, [
   body('title', 'Enter a valid title').isLength({ min:3 }),
@@ -43,5 +43,34 @@ catch (error) {
   res.status(500).send("Internal server error"); 
 }
 })
+
+
+// Route 3  To Update the existing note with /api/notes/updatenote login required
+router.put('/updatenote/:id',fetchuser, async (req, res) => {
+try {
+  const { title, description, tag } = req.body;
+   // create a new note object.......
+   const newNote = {};
+   if(title){newNote.title = title};
+   if(description){newNote.description = description};
+   if(tag){newNote.tag = tag};
+
+   // Find the note to be updated and update it.........
+   let note = await Notes.findById(req.params.id);
+   if(!note){return res.status(404).send("Not Found")}
+
+   if(note.user.toString() !== req.user.id){
+    return res.status(401).send("Not Allowed");
+   }
+
+   note = await Notes.findByIdAndUpdate(req.params.id, {$set:newNote}, {new:true});
+  res.json(note)
+}
+catch (error) {
+  console.error(error.message);
+  res.status(500).send("Internal server error"); 
+}
+})
+
 
 export default router;
