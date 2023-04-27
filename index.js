c7cc4e14b06cb2a35861c1f5ffc14635be6de6a0
madhur <mad@gmail.com>
2023-04-27 20:32:25 +0530

    updated index.js

--- index.js
+++ index.js
@@ -2,13 +2,14 @@ import connectToMongo from "./db.js";
 import express from "express";
 import authRoutes from './routes/auth.js';
 import notesRoutes from './routes/notes.js';
+import cors from 'cors';
 
 
 connectToMongo();
 const app = express()
 const port = 5000
 app.use(express.json());
-
+app.use(cors());
 // AVAILABLE ROUTES
 app.use('/api/auth', authRoutes);
 app.use('/api/notes', notesRoutes);
